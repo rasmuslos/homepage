160dd3ce9041b45a9c27c71330c1e0ef9b0f1b0e
Rasmus Krämer <git@rfk.io>
2024-03-30 10:03:26 +0100

    Small improvements

--- static/scripts/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-    document.querySelector("input").addEventListener("keydown", event => {
-        const query = event.target.value.trim()
-
-        if(event.key !== "Enter" || query === "") return
-        window.location.href = `https://duckduckgo.com/?q=${query}`
-    })
-})
